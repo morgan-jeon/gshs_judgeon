42ea11bf10239f8d8e74a813695e860abd3327b0
Morgan_KR <jeonmogeon@gmail.com>
2021-05-14 18:31:53 +0900

    aa

--- Tools/autoCommit.py
+++ Tools/autoCommit.py
@@ -1,1 +1,55 @@
 import requests
+
+def submit(req, submit_dict):
+	url = 'http://judgeon.net/submit.php'
+
+	for su in submit_dict.keys():
+		dict_data = {'id':su,'language':'1','source':submit_dict[su],'csrf':''}
+
+		response = req.post(url=url, data=dict_data, headers={'Content-Type': 'application/x-www-form-urlencoded','Cookie':'PHPSESSID=5ise9ata42likdrfqau0v9u967'})
+
+		if response.status_code == 200:
+			print(f'Problem id {su} Submitted')
+		# print(response.content.decode())
+
+def login(req):
+	url = 'http://judgeon.net/login.php'
+
+	dict_data = {'user_id':'gshs363315','password':'979f8a033625f761b3ef4807bce2a837','submit':'','csrf':''}
+
+	response = req.post(url=url, data=dict_data, headers={'Content-Type': 'application/x-www-form-urlencoded','Cookie':'PHPSESSID=5ise9ata42likdrfqau0v9u967'})
+
+	if response.status_code == 200:
+		print("Login Succeed")
+
+
+if __name__=="__main__":
+	s = requests.Session()
+	login(s)
+	submit_dict = {'2010':'''
+	#include <cstdio>
+	#include <stack>
+
+	int main(){
+	    int n, r=0;
+	    scanf("%d",&n);
+	    std::stack<int> s;
+	    for(int i=0,tmp;i<n;i++){
+	    	scanf("%d", &tmp);
+	    	s.push(tmp);
+	    }
+	    int tmp;
+		while(!s.empty()){
+			if(tmp>s.top()){
+				r-=s.top();
+			}
+			else {
+				r+=s.top();
+			}
+			tmp=s.top();
+	        s.pop();
+	    }
+	    printf("%d", r);
+	}
+	'''}
+	submit(s, submit_dict)
--- Tools/cheatCode.py
+++ Tools/cheatCode.py
@@ -1,1 +1,49 @@
 import requests
+import time
+
+def submit(req, submit_dict, delay):
+	url = 'http://judgeon.net/submit.php'
+
+	for su in submit_dict.keys():
+		dict_data = {'id':su,'language':'1','source':submit_dict[su],'csrf':''}
+
+		time.sleep(delay)
+
+		response = req.post(url=url, data=dict_data, headers={'Content-Type': 'application/x-www-form-urlencoded','Cookie':'PHPSESSID=5ise9ata42likdrfqau0v9u967'})
+
+		if response.status_code == 200:
+			print(f'Problem id {su} Submitted')
+		# print(response.content.decode())
+
+def login(req):
+	url = 'http://judgeon.net/login.php'
+
+	# dict_data = {'user_id':'gshs363315','password':'979f8a033625f761b3ef4807bce2a837','submit':'','csrf':''}
+	
+	dict_data = {'user_id':'testtesttest','password':'05a671c66aefea124cc08b76ea6d30bb','submit':'','csrf':''}
+
+	response = req.post(url=url, data=dict_data, headers={'Content-Type': 'application/x-www-form-urlencoded','Cookie':'PHPSESSID=5ise9ata42likdrfqau0v9u967'})
+
+	if response.status_code == 200:
+		print("Login Succeed")
+
+
+if __name__=="__main__":
+	s = requests.Session()
+	login(s)
+	submit_dict = dict()
+	f = open('ac-gshs361508.txt','r',-1,'utf-8')
+	while 1:
+		a = f.readline()
+		# print(a)
+		if not a: 
+			break
+		if 'Problem' in a:
+			pid = int(a.replace('Problem','').replace(':',''))
+			print(f'{pid}')
+			submit_dict[pid] = ''
+		elif '-------' not in a:
+			submit_dict[pid] = submit_dict[pid] + a
+
+	print(len(submit_dict))
+	submit(s, submit_dict, 1)
